fc36e948265178ca0c964c2bd44091a2527af82d
Antoine Bouabana <abouaban2@gmail.com>
2021-05-14 12:21:32 +0200

    prettify files + eslint fix import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,11 +8,11 @@
 
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
-import { HomePage } from './pages/HomePage/Loadable';
-import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { HomePage } from './pages/HomePage/Loadable';
 
 export function App() {
     const { i18n } = useTranslation();
