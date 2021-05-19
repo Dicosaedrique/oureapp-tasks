1a178aeab760f54926b529a039e8b6f8e0235b40
Antoine Bouabana <abouaban2@gmail.com>
2021-05-19 23:43:55 +0200

    rework filtering system (inclusion -> exclusion

--- src/model/Task/Filter.ts
+++ src/model/Task/Filter.ts
@@ -7,14 +7,14 @@ import { TaskPriority } from './Priority';
 
 /**
  * Defines settings to filter tasks (by state, priority, limit date and category).
- * Filters logic is based on array of accepted value, if none or
- * all are selected then the array is empty and filters is ignored.
+ * Filters logic is based on array of values to exclude, if array is empty, all values
+ * are accepted.
  */
 export interface FilteringSettings {
     state: Array<TaskState>;
     priority: Array<TaskPriority>;
-    limitDate: Array<boolean>; // true = only with limit date, false = only without limit date
-    category: Array<CategoryID>;
+    limitDate: Array<boolean>; // true = exclude tasks with limit date, false = exclude tasks without limitdate
+    category: Array<CategoryID | undefined>; // accepts undefined for tasks with no category
 }
 
 /**
@@ -26,55 +26,57 @@ export type FilterableTaskKeys = keyof FilteringSettings;
 // DEFINES EVERY FILTER
 
 /**
- * Memoized function that returns a filter that accepts every tasks with a state in the values
- * if the values are empty, accepts all tasks
- * @param values states accepted in the filter
+ * Memoized function that returns a filter that rejects every tasks with a state in the values
+ * @param values states rejected in the filter
  * @returns the filter for task state
  */
 function filterState(values: TaskState[]): Filter<Task> {
     return memoize(
-        (task: Task) => values.length === 0 || values.includes(task.state),
+        (task: Task) => values.length === 0 || !values.includes(task.state),
     );
 }
 
 /**
- * Memoized function that returns a filter that accepts every tasks with a priority in the values
- * if the values are empty, accepts all tasks
- * @param values priorities accepted in the filter
+ * Memoized function that returns a filter that rejects every tasks with a priority in the values
+ * @param values priorities rejected in the filter
  * @returns the filter for task priority
  */
 function filterPriority(values: TaskPriority[]): Filter<Task> {
     return memoize(
-        (task: Task) => values.length === 0 || values.includes(task.priority),
+        (task: Task) => values.length === 0 || !values.includes(task.priority),
     );
 }
 
 /**
- * Memoized function that returns a filter that accepts every tasks with or without
- * a limit date based on the first element of the values array (this is the only filter that does
- * that because it's a boolean so only one value is accepted). if the values are empty, accepts all tasks
- * @param values array of one boolean or empty
+ * Memoized function that returns a filter that rejects every tasks with or without a limit date
+ * @param values array of boolean (only one value is usefull, true to reject tasks with limit date and false for the contrary)
  * @returns the filter for task limit date
  */
-function filterLimitDate([value]: boolean[]): Filter<Task> {
+function filterLimitDate(values: boolean[]): Filter<Task> {
     return memoize((task: Task) => {
-        if (value === true) return task.limitDate !== undefined;
-        else if (value === false) return task.limitDate === undefined;
-        return true;
+        if (values.length === 0) return true;
+
+        const excludeTasksWithLimitDate = values.includes(true);
+        const excludeTasksWithoutLimitDate = values.includes(false);
+        const excludeAll =
+            excludeTasksWithLimitDate && excludeTasksWithoutLimitDate;
+
+        return (
+            !excludeAll &&
+            ((excludeTasksWithLimitDate && task.limitDate === undefined) ||
+                (excludeTasksWithoutLimitDate && task.limitDate !== undefined))
+        );
     });
 }
 
 /**
- * Memoized function that returns a filter that accepts every tasks with a category in the values
- * if the values are empty, accepts all tasks
- * @param values categories accepted in the filter
+ * Memoized function that returns a filter that rejects every tasks with a category in the values
+ * @param values categories rejected in the filter
  * @returns the filter for task category
  */
-function filterCategory(values: string[]): Filter<Task> {
+function filterCategory(values: (string | undefined)[]): Filter<Task> {
     return memoize(
-        (task: Task) =>
-            values.length === 0 ||
-            (task.category !== undefined && values.includes(task.category)),
+        (task: Task) => values.length === 0 || !values.includes(task.category),
     );
 }
 
