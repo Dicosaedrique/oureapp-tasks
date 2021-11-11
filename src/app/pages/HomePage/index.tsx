f646ccf2373726d84b70d4c4a09709e77549feb1
Antoine Bouabana <abouaban2@gmail.com>
2021-11-10 22:31:56 -0500

    add title to home page

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -2,11 +2,15 @@ import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 export function HomePage() {
     return (
         <Container component="main">
+            <Helmet>
+                <title>Welcome to Oureapp Task</title>
+            </Helmet>
             <CssBaseline />
             <Typography variant="h3" style={{ textAlign: 'center', margin: '1em 0' }}>
                 Welcome to the todo app !
