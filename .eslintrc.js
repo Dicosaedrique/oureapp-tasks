3c2cb232a3475eab7cfebec258450f45dbc5a6e5
Antoine Bouabana <abouaban2@gmail.com>
2021-11-15 23:37:05 -0500

    update packages and switch from mui4 to 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,6 @@ module.exports = {
     plugins: ['simple-import-sort', 'prettier'],
     rules: {
         'prettier/prettier': ['warn', prettierOptions],
-        'no-restricted-imports': [
-            'error',
-            {
-                patterns: ['@material-ui/*/*/*', '!@material-ui/core/test-utils/*'],
-            },
-        ],
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
         '@typescript-eslint/no-non-null-assertion': 0,
